66936fa16443851870bcaa8dd21e66ce21d8dbba
Sachin Ramteke <sachin06jd@gmail.com>
2020-06-29 16:57:47 -0500

    finished Subtraction function

--- js/script.js
+++ js/script.js
@@ -7,5 +7,10 @@ let calculator = {
         let sum = int1 + int2;
         // console.log(`Add Calculator ${int1} and ${int2}`);
         return sum;
+    },
+    Subtraction: function (num1, num2) {
+        let diff = num1 - num2;
+        console.log(`diff is ${diff}`);
+        return diff;
     }
 };
