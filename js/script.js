d3a645b309902522e8431d038b4c43b7e83cbdc4
Sachin Ramteke <sachin06jd@gmail.com>
2020-06-29 16:52:34 -0500

    finished Addition function

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("script file is linked");
+
+//homework - create javascript calculator
+
+let calculator = {
+    Addition: function (int1, int2) {
+        let sum = int1 + int2;
+        // console.log(`Add Calculator ${int1} and ${int2}`);
+        return sum;
+    }
+};
